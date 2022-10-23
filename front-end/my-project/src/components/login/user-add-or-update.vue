3a9e23059486b03ec5d42e2d9f1fe8dfd5df2b16
Frankie Feng <56572346+ffen126@users.noreply.github.com>
2022-10-23 23:16:03 +1300

    Update user-add-or-update.vue

--- front-end/my-project/src/components/login/user-add-or-update.vue
+++ front-end/my-project/src/components/login/user-add-or-update.vue
@@ -63,7 +63,7 @@
               }
             })
       },
-      // 表单提交
+      // Form submission
       dataFormSubmit () {
         this.$refs['dataForm'].validate((valid) => {
           if (valid) {
